b0eea9588aca1b0403c8c3282057cddabc7db37d
Jorge Garit <jorgegarit@gmail.com>
2022-09-20 13:13:27 -0400

    hashing added

--- models/User.js
+++ models/User.js
@@ -1,14 +1,20 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
   {
     // TABLE COLUMN DEFINITIONS GO HERE
-    
+
     // define an id column
     id: {
         // use the special Sequelize DataTypes object provide what type of data it is
@@ -46,7 +52,21 @@ User.init(
         }
       }
   },
+
   {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+        },
+         // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+        }
+    },
+
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
@@ -59,7 +79,8 @@ User.init(
     underscored: true,
     // make it so our model name stays lowercase in the database
     modelName: 'user'
-  }
+    }
+
 );
 
 module.exports = User;
