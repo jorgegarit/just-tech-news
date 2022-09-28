5466758d464cb1cc6d6e0c6f9c66753b72f77f90
Jorge Garit <jorgegarit@gmail.com>
2022-09-27 21:49:37 -0400

    added upvote functionality:

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
-const { restore } = require('../../models/Post');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req,res) => {
     console.log('===============')
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post.id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -29,7 +35,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post.id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -63,6 +75,17 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
       });
 });
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 
 router.put('/:id', (req, res) => {
     Post.update(
